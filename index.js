dfb0a94715b82cd2bec9b4a5d0de6986c7d356a0
Alexhabig <markaeroltomarse>
2022-08-17 22:11:03 +0800

    socket for chat app

--- index.js
+++ index.js
@@ -3,29 +3,50 @@ const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require("socket.io")
-const io = new Server(server);
-
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-  });
-
-io.on('connection', (socket) => {
-    console.log('a user connected gago');
-
-    socket.on('chat message', (msg)=>{
-        io.emit('chat message', msg);
-        console.log("message: " + msg);
-    });
-    socket.on('disconnect', ()=>{
-        console.log("disconnected kana gago")
-    });
-
+const io = new Server(server,{
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  }
 });
 
+io.on('connection', (socket)=>{
+  socket.on("getMessage", (data)=>{
+    console.log("get message: " + JSON.stringify(data))
+  })
+  console.log("socket connected")
+})
+
+server.listen(3001,()=>{
+  console.log("server run port 3001")
+})
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// const { Server } = require("socket.io")
+// const io = new Server(server);
 
 
-server.listen(3000, () => {
-  console.log('tang ina mo listening on *:3000');
-  console.log('tang inamo')
-});
+// // app.get('/', (req, res) => {
+// //     res.sendFile(__dirname + '/index.html');
+// //     // res.links('http://localhost:3000/');
+// //   });
+
+// io.on('connection', (socket) => {
+//     console.log('a user connected gago');
+
+//     socket.on('chat message', (msg)=>{
+//         io.emit('chat message', msg);
+//         console.log("message: " + msg);
+//     });
+//     socket.on('disconnect', ()=>{
+//         console.log("disconnected kana gago")
+//     });
+
+// }); 
+
+// server.listen(3001, () => {
+//   console.log('tang ina mo listening on *:5000');
+//   console.log('tang inamo')
+// });
